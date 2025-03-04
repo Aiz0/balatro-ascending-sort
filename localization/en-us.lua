afc249d7671a4a65d68c4bf55d4d772bde04afce
Aiz <dev@aiz.moe>
2025-03-04 19:07:25 +0100

    feat: use localization file

---
+++ localization/en-us.lua
@@ -0,0 +1,13 @@
+return {
+    misc = {
+        dictionary = {
+            AizAscendingSort_default_sort = "Default sort",
+        },
+        AizAscendingSort_options = {
+            desc = "Descending",
+            asc = "Ascending",
+            ["suit desc"] = "Suit descending",
+            ["suit asc"] = "Suit ascending",
+        },
+    },
+}
--- main.lua
+++ main.lua
@@ -12,6 +12,10 @@ G.FUNCS.AizAscendingSort_callback = function(args)
 end
 
 SMODS.current_mod.config_tab = function()
+    local loc_options = {}
+    for i, v in ipairs(config.default_sort.options) do
+        loc_options[i] = localize(v, "AizAscendingSort_options")
+    end
     return {
         n = G.UIT.ROOT,
         config = {
@@ -24,12 +28,13 @@ SMODS.current_mod.config_tab = function()
         },
         nodes = {
             create_option_cycle({
-                label = "Default sort",
+                label = localize("AizAscendingSort_default_sort"),
                 current_option = config.default_sort.value,
-                options = config.default_sort.options,
+                options = loc_options,
                 ref_table = config.default_sort,
                 ref_value = "value",
                 opt_callback = "AizAscendingSort_callback",
+                w = 6,
             }),
         },
     }
